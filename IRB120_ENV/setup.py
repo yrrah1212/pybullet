1923697c2ef25bcf0ae6ffc7bfb3c18a29ef9acc
Harry Dodwell <yrrah1212@gmail.com>
2021-10-26 14:41:51 -0600

    removed redundancies

--- IRB120_ENV/setup.py
+++ IRB120_ENV/setup.py
@@ -1,6 +1,3 @@
-# Credit to: https://gerardmaggiolino.medium.com/creating-openai-gym-environments-with-pybullet-part-2-a1441b9a4d8e
-# Also used: https://github.com/openai/gym/blob/master/docs/creating_environments.md
-
 from setuptools import setup
 
 setup(
--- q_learning.py
+++ q_learning.py
@@ -5,46 +5,31 @@ import tensorflow as tf
 from tensorflow.python.ops.gen_math_ops import tanh_grad_eager_fallback
 
 from tf_agents.agents.dqn import dqn_agent
-from tf_agents.drivers import dynamic_step_driver
+from tf_agents.drivers import py_driver
+from tf_agents.environments import suite_gym
 from tf_agents.environments import tf_py_environment
 from tf_agents.eval import metric_utils
 from tf_agents.metrics import tf_metrics
-from tf_agents.networks import q_network
-from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.networks import sequential
+from tf_agents.policies import py_tf_eager_policy
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
 from tf_agents.trajectories import trajectory
+from tf_agents.specs import tensor_spec
 from tf_agents.utils import common
 
 # TODO Need to implement an agent
 
 def main():
-    env = gym.make("IRB120-v0")
+    train_env = suite_gym.load("IRB120-v0")
+    test_env = suite_gym.load("IRB120-v0")
 
     # Convert env numpy arrays to tensors for tensorflow 
-    env = tf_py_environment.TFPyEnvironment(env)
-    q_net = q_network.QNetwork(env.observation_spec(), env.action_spec)
+    train_env = tf_py_environment.TFPyEnvironment(train_env)
+    test_env = tf_py_environment.TFPyEnvironment(test_env)
 
-    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=.001)
-
-    train_step_counter = tf.Variable(0)
-
-    agent = dqn_agent(env.time_step_spec(),
-                        env.action_spec(),
-                        optimizer=optimizer,
-                        td_errors_loss_fn=common.element_wise_squared_loss,
-                        train_step_counter=train_step_counter)
-
-
-    for i in range(10):
-        observation = env.reset()
-        for t in range(10000):
-            action = env.action_space.sample()
-            # print(env.prev_error)
-            observation, reward, done, info = env.step(action)
-            if done:
-                print(f"Feached the target in {t} steps")
-                break
-
-    env.close()
+    
 
 
 if __name__ == "__main__":
