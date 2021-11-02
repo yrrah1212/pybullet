99d94f5f9fbbce3f86ecf45ce1e01efa018d0686
hmdod <yrrah1212@gmail.com>
2021-11-02 16:17:56 -0600

    Update __init__.py

--- IRB120_ENV_simple/irb120_env_simple/__init__.py
+++ IRB120_ENV_simple/irb120_env_simple/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='IRB120-simple-v0', 
-    entry_point='irb120_env.envs:IRB120ENV'
+    entry_point='irb120_env_simple.envs:IRB120ENV_simple'
 )
