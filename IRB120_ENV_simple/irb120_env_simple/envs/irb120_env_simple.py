140036165c9285bb0ed594f59e12e920fab3567a
yrrah1212 <harry@proteussnowboards.com>
2021-11-25 12:11:43 -0500

    Update irb120_env_simple.py

--- IRB120_ENV_simple/irb120_env_simple/envs/irb120_env_simple.py
+++ IRB120_ENV_simple/irb120_env_simple/envs/irb120_env_simple.py
@@ -21,8 +21,8 @@ class IRB120ENV_simple(gym.Env):
         )
 
         self.observation_space = gym.spaces.box.Box(
-            low=-2.87979,
-            high=2.87979
+            low=np.array([-2.87979]),
+            high=np.array([2.87979])
         )
         
         self.seed()
